c2354f61f8204e0584aa40369ca85a8c64c87477
Alan Silva <60674021+AlanMSilva@users.noreply.github.com>
2020-02-23 16:18:45 -0600

    Add files via upload

---
+++ serie de cos.js
@@ -0,0 +1,24 @@
+let x=1,y=2,s=y,c=6,l=1;
+    for(i = 1; i<=7;i++)
+    {
+        x*=i;
+        y=Math.pow(y,i);
+        if(i%2==0)
+        {
+            if(l==1)
+            {
+                s+=y/x;
+                l--;
+            }
+            else
+            {
+                s-=y/x;
+                l++;
+            }           
+        }
+        y=2;
+    }
+console.log(s-1);
+
+
+

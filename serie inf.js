a298218bb6d0b70237a0067460c4caaea05acace
Alan Silva <60674021+AlanMSilva@users.noreply.github.com>
2020-02-24 18:52:32 -0600

    Add files via upload

--- serie inf.js
+++ serie inf.js
@@ -1,9 +1,9 @@
-var x=2,s=0,s2=0,frac=0,suma;
-for(var i = 2; i<=6;i++)
+var x=2,s=0,s2=0,frac=0,suma=0;
+for(var i = 1; i<=3;i++)
 { 
-    s=x-1/x;
+    s=(x-1)/x;
     frac=1/i;
     suma=Math.pow(s,i)
-    s2+=frac*s;
+    s2+=frac*suma;
 }
 console.log(s2)
